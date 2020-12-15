fafe002880c8bcb88f9d41cd98280da246ceedee
Saurabh <saurabhj8087445@gmail.com>
2020-12-15 17:26:15 +0530

    Adding recommended files and other stuff

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,11 +1,86 @@
 import React from "react";
 import "./RecommendedVideos.css";
+import VideoCard from "./VideoCard";
 
 function RecommendedVideos() {
-  return( 
-  <div className="recommendedVideos">
-        
-  </div>
+  return (
+    <div className="recommendedVideos">
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          image="https://i.ytimg.com/an_webp/U9T6YkEDkMo/mqdefault_6s_480x270.webp?du=3000&sqp=CN6F4v4F&rs=AOn4CLCFxBrh35bZ2ilPy9fa3BENYZ4u-g"
+          title="Build a Recipe App With React | React Tutorial For Beginners"
+          views="2.3M"
+          timestamp="1 year ago"
+          channel="Dev Ed"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhqOT2rmcUlS-pOBQeg8m4GCYTXT87VKNoyyg=s68-c-k-c0x00ffffff-no-rj-mo"
+        />
+        <VideoCard
+          image="https://i.ytimg.com/an_webp/pZP4VP9MCIA/mqdefault_6s_480x270.webp?du=3000&sqp=COCR4v4F&rs=AOn4CLDP3m_HPT4NbL_O0SFpmb8TlzJldQ"
+          title="Dance+ 4 | Dilbar"
+          views="33M"
+          timestamp="4 days ago"
+          channel="StarPlus"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwnhm3-Fe1-4j2pNi3OA8jVhQiwbxJSXSS_En2tOGJA4=s88-c-k-c0xffffffff-no-rj-mo"
+        />
+        <VideoCard
+          image="https://i.ytimg.com/an_webp/-MlNBTSg_Ww/mqdefault_6s.webp?du=3000&sqp=CKCV4v4F&rs=AOn4CLDtmB32yNcr8-uFbWZwlBXkcAuGQw"
+          title="React Hooks| React Tutorial For Beginners"
+          views="456K"
+          timestamp="3 months ago"
+          channel="Academind"
+          channelImage="https://yt3.ggpht.com/a-/AOh14Gj286c5gsHOdd8GjPlLu-NdKL79PGJjFhrtwQ=s68-c-k-c0x00ffffff-no-rj-mo"
+        />
+        <VideoCard
+          image="https://i.ytimg.com/an_webp/1ckRu1dTVIY/mqdefault_6s.webp?du=3000&sqp=CIif4v4F&rs=AOn4CLCxXGqCTBKawTvkxzGDIo0MO_Euyw"
+          title="Our Government is committed to farmer welfare: PM Modi"
+          views="44M"
+          timestamp="1 year ago"
+          channel="Narendra Modi"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwnjBxDbxCCpVNyEEKREl0qhQcIJ8DNaJkpv57LDsCMs=s88-c-k-c0xffffffff-no-rj-mo"
+        />
+        <VideoCard
+          image="https://i.ytimg.com/an_webp/HO7CRp44s10/mqdefault_6s.webp?du=3000&sqp=COSq4v4F&rs=AOn4CLCz2otbgo4qDjMRXoda56vyV98rtg"
+          title="First Kiss: Yo Yo Honey Singh Ft. Ipsitaa | Bhushan Kumar | Lil Golu, Singhsta, Hommie D, DirGifty"
+          views="89M"
+          timestamp=" Nov 24,2020"
+          channel="T-Series"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwniNTDIUibNIZwwUWC820n6i3yPz-rnBDwOpZED1CQY=s88-c-k-c0xffffffff-no-rj-mo"
+        />
+        <VideoCard
+          image="https://i.ytimg.com/an_webp/R3ts_5LIqWw/mqdefault_6s.webp?du=3000&sqp=CICX4v4F&rs=AOn4CLBF3PH0mMJ1G8i4vRppHQMObb9nLA"
+          title="George Calombaris takes the Jalebi Challenge"
+          views="233K"
+          timestamp="3 year ago"
+          channel="Zomato"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwngxN3Uw7rPD-0NKtgfyGRbgb36mXWTFvPe4kFvpqw=s176-c-k-c0x00ffffff-no-rj"
+        />
+        <VideoCard
+          image="https://i.ytimg.com/an_webp/-MlNBTSg_Ww/mqdefault_6s.webp?du=3000&sqp=CKCV4v4F&rs=AOn4CLDtmB32yNcr8-uFbWZwlBXkcAuGQw"
+          title="React Hooks| React Tutorial For Beginners"
+          views="456K"
+          timestamp="3 months ago"
+          channel="Academind"
+          channelImage="https://yt3.ggpht.com/a-/AOh14Gj286c5gsHOdd8GjPlLu-NdKL79PGJjFhrtwQ=s68-c-k-c0x00ffffff-no-rj-mo"
+        />
+        <VideoCard
+          image="https://i.ytimg.com/an_webp/U9T6YkEDkMo/mqdefault_6s_480x270.webp?du=3000&sqp=CN6F4v4F&rs=AOn4CLCFxBrh35bZ2ilPy9fa3BENYZ4u-g"
+          title="Build a Recipe App With React | React Tutorial For Beginners"
+          views="2.3M"
+          timestamp="1 year ago"
+          channel="Dev Ed"
+          channelImage="https://yt3.ggpht.com/a-/AOh14GhqOT2rmcUlS-pOBQeg8m4GCYTXT87VKNoyyg=s68-c-k-c0x00ffffff-no-rj-mo"
+        />
+        <VideoCard
+          image="https://i.ytimg.com/an_webp/HO7CRp44s10/mqdefault_6s.webp?du=3000&sqp=COSq4v4F&rs=AOn4CLCz2otbgo4qDjMRXoda56vyV98rtg"
+          title="First Kiss: Yo Yo Honey Singh Ft. Ipsitaa | Bhushan Kumar | Lil Golu, Singhsta, Hommie D, DirGifty"
+          views="89M"
+          timestamp=" Nov 24,2020"
+          channel="T-Series"
+          channelImage="https://yt3.ggpht.com/ytc/AAUvwniNTDIUibNIZwwUWC820n6i3yPz-rnBDwOpZED1CQY=s88-c-k-c0xffffffff-no-rj-mo"
+        />
+      </div>
+    </div>
   );
 }
 
